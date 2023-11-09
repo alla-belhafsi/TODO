124b30abcc7cba7126fd484575c34ccbf6d73932
Alla Belhafsi <alla.belhafsi@stagiaire-elan.fr>
2023-11-09 11:25:55 +0100

    working css

--- script.js
+++ script.js
@@ -1,31 +1,59 @@
+// Sélectionnez l'élément de modèle initial de la tâche et le conteneur des tâches
 const taskCard = document.querySelector(".todoCard");
-const tasksContainer = document.querySelector("#todoCards");// add a task on click
+const tasksContainer = document.querySelector("#todo");
 
+// Sélectionne le bouton "Add a task" et ajoute un gestionnaire d'événement de clic
 const addBtn = document.querySelector('#btn');
 addBtn.addEventListener('click', addTask);// add a task on click
 
+// Sélectionne le bouton de suppression (delBtn) faut ajuster ce sélecteur en fonction de la structure HTML
 const delBtn = document.querySelector('.delBtn');
-delBtn.addEventListener('click', function() {// delete default task on click
-    deleteTask(taskCard); // target the right task
+delBtn.addEventListener('click', function() {
+    deleteTask(taskCard); // Supprime le modèle initial
 });
 
+// Fonction pour créer une nouvelle tâche en clonant le modèle initial
+function createTask() {
+    const newTask = taskCard.cloneNode(true);
+    return newTask;
+}
 
+// Fonction pour ajouter une nouvelle tâche
 function addTask() {
-    const newTask = taskCard.cloneNode(true)// clone the task card
-    const newDelBtn = newTask.querySelector('.delBtn')
-    const newTextArea = newTask.querySelector('.task')
+    // Crée une nouvelle tâche en clonant le modèle initial
+    const newTask = createTask();
 
-    newTextAres.value = "New Task"// set new task text to "New Task"
-    newDelBtn.addEventListener('click', function(){ // add delete event listener to new task
-        deleteTask(newTask); // target the new task
+    // Sélectionne les éléments de la nouvelle tâche
+    const newDelBtn = newTask.querySelector('.delBtn');
+    const newTextArea = newTask.querySelector('.task');
+
+    // Définir le texte de la nouvelle tâche sur "New Task"
+    newTextArea.value = "New Task";
+    
+    // Ajoutez un gestionnaire d'événement de clic pour la suppression de la tâche
+    newDelBtn.addEventListener('click', function(){ 
+        deleteTask(newTask); 
     });
 
-    tasksContainer.appendChild(newTask)// apprend new task to the tasks container
+    // Ajoutez la nouvelle tâche au conteneur des tâches
+    tasksContainer.appendChild(newTask);
+
+    // Mettez à jour le compteur du nombre de tâches
     updateCount();
 }
 
-
-
+// Fonction pour supprimer une tâche
 function deleteTask(task){
-    task.remove(); // remove the task
+    task.remove(); 
+
+    // Mettez à jour le compteur du nombre de tâches
+    updateCount();
+}
+
+// Fonction pour mettre à jour le compteur du nombre de tâches
+function updateCount() {
+    // Exclure le modèle initial du comptage
+    const taskCount = tasksContainer.querySelectorAll('.todoCard').length;
+    const countElement = document.querySelector("#count");
+    countElement.textContent = taskCount + " task" + (taskCount !== 1 ? "s" : "");
 }
